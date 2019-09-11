4a4275f3d68e6e504130deb8bb7870134cc6d9e7
Lukas <lukas.frantzke@clockwork.ca>
2019-09-11 13:45:06 -0400

    Initial Commit

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Lukas on 2019-09-11.
+//  Copyright © 2019 Lukas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
